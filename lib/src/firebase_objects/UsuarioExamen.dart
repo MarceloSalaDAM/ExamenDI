788f9c408dfd8103191807e192839d692088bb45
Marcelo Sala <dam.marcelo.dosala@gmail.com>
2022-11-21 13:51:52 +0100

    creacion del FbAdmin para poder descargar los datos porque da error el UsuarioExamen

--- lib/src/firebase_objects/UsuarioExamen.dart
+++ lib/src/firebase_objects/UsuarioExamen.dart
@@ -1,12 +1,29 @@
+
 class UsuarioExamen {
   final String? nombre;
   final String? apellidos;
 
   UsuarioExamen({
-    this.nombre="",
-    this.apellidos="",
+    this.nombre = "",
+    this.apellidos = "",
 
-});
+  });
 
+  factory UsuarioExamen.fromFirestore(
+      DocumentSnapshot<Map<String, dynamic>> snapshot,
+      SnapshotOptions? options,) {
+    final data = snapshot.data();
+    return UsuarioExamen(
+        nombre: data?['nombre'],
+        apellidos: data?['apellidos'],
 
+    }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      if (nombre != null) "nombre": nombre,
+      if (apellidos != null) "ciudad": apellidos,
+
+    };
+  }
 }
