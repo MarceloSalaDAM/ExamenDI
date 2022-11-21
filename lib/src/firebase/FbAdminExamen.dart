c51b4f3e1ccce0046ec973c521cff1a99c5aa1b7
Marcelo Sala <dam.marcelo.dosala@gmail.com>
2022-11-21 13:53:13 +0100

    FbAdmin creado

---
+++ lib/src/firebase/FbAdminExamen.dart
@@ -0,0 +1,22 @@
+import 'package:examen_din1/src/firebase_objects/UsuarioExamen.dart';
+
+class FbAdminExamen {
+  FbAdminExamen() {
+
+  }
+  Future<UsuarioExamen?> descargarPerfil(String? idPerfil) async {
+    FirebaseFirestore db=FirebaseFirestore.instance;
+    final docRef = db
+        .collection("usuarios")
+        .doc(idPerfil)
+        .withConverter(
+        fromFirestore: UsuarioExamen.fromFirestore,
+        toFirestore: (UsuarioExamen usuario, _) => usuario.toFirestore());
+
+    final docSnap = await docRef.get();
+    return docSnap.data();
+
+
+  }
+
+}
