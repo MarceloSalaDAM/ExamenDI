f30fb7b6da77fc9cd6441f7ec0f34d60c934fe23
Marcelo Sala <dam.marcelo.dosala@gmail.com>
2022-11-21 12:04:38 +0100

    creacion clase app para modificar el main

---
+++ lib/src/App.dart
@@ -0,0 +1,18 @@
+import 'package:examen_din1/src/login_views/LoginViewExamen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //declaramos las rutas que va a seguir nuetsra aplicación
+    return MaterialApp(
+      initialRoute: '/Login',
+      routes: {
+        '/Login': (context) => LoginViewExamen(),
+      },
+    );
+  }
+}
