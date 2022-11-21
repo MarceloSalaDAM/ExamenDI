f0a27748456590eb7e76b71a25897f36ee6fe716
Marcelo Sala <dam.marcelo.dosala@gmail.com>
2022-11-21 12:55:42 +0100

    homeview creado

---
+++ lib/src/home_views/HomeViewExamen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeViewExamen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeViewExamenState();
+  }
+}
+
+class _HomeViewExamenState extends State<HomeViewExamen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("HOME VIEW EXAMEN"),
+        //esta barra de código es para quitar el icono de la flecha arriba a la izquierda de mi aplicacion
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.black,
+      ),
+      body: Center(child: Column()),
+      backgroundColor: Colors.white,
+    );
+  }
+}
