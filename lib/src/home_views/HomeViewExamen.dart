d6e6528ebfb993e46049dad58d9b7b4d758f0f73
Marcelo Sala <dam.marcelo.dosala@gmail.com>
2022-11-24 10:21:55 +0100

    homeview actualizado para que muestre la lista

--- lib/src/home_views/HomeViewExamen.dart
+++ lib/src/home_views/HomeViewExamen.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:examen_din1/src/singleton/DataHolderExamen.dart';
 import 'package:flutter/material.dart';
 
 import '../firebase_objects/BichosExamen.dart';
@@ -38,10 +39,9 @@ class _HomeViewExamenState extends State<HomeViewExamen> {
 
   void listItemShortClicked(int index) {
     print("CR7: " + bichos[index].tipo!);
-    DataHolder2().selectedChatRoom = chatRooms[index];
+    DataHolderExamen().selectedBicho = bichos[index];
     Navigator.of(context).pushNamed("/CR7");
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -52,24 +52,17 @@ class _HomeViewExamenState extends State<HomeViewExamen> {
         automaticallyImplyLeading: false,
         backgroundColor: Colors.black,
       ),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              OutlinedButton(
-                onPressed: () {
-                  Navigator.of(context).popAndPushNamed('/Login');
-                  ;
-                },
-                child: Text("ACEPTAR"),
-              )
-            ],
-          )
-        ],
-      )),
+      body: ListView.separated(
+        padding: const EdgeInsets.all(10),
+        itemCount: bichos.length,
+        itemBuilder: (BuildContext context, int index) {
+          return BichosItemExamen(
+              tituloLista: bichos[index].tipo!,
+              onShortClick: listItemShortClicked,
+              index: index);
+        },
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
+      ),
       backgroundColor: Colors.white,
     );
   }
